ed8ab3089f1d76c46c4cba5845e58d8d72e4a7cf
YaBLo4nY-Rab <TambovtsevDanila123@gmail.com>
2023-03-13 15:24:05 +0400

    #

--- project/MainWindow.gd
+++ project/MainWindow.gd
@@ -8,16 +8,55 @@ func _ready():
 
 
 func _on_FileDialog_file_selected(path):
-	$MarginContainer/VBoxContainer/HBoxContainer/LineEdit.text = path
+	print("file nimber", file_selecting)
+	print(path)
+	
+	var size_f: String
+	
+	var f := File.new()
+	f.open(path, File.READ)
+	size_f = String(f.get_len())
+	f.close()
+	
+	print(size_f)
+	
+	
+	if file_selecting == 1:
+		$MarginContainer/VBoxContainer/HBoxContainer/LineEdit.text = path
+		$MarginContainer/VBoxContainer/HBoxContainer/LineEdit2.text = size_f
+	elif file_selecting == 2:
+		$MarginContainer/VBoxContainer/HBoxContainer2/LineEdit.text = path
+		$MarginContainer/VBoxContainer/HBoxContainer2/LineEdit2.text = size_f
+	elif file_selecting == 3:
+		$MarginContainer/VBoxContainer/HBoxContainer3/LineEdit.text = path
+		$MarginContainer/VBoxContainer/HBoxContainer3/LineEdit2.text = size_f
+	elif file_selecting == 4:
+		$MarginContainer/VBoxContainer/HBoxContainer4/LineEdit.text = path
+		$MarginContainer/VBoxContainer/HBoxContainer4/LineEdit2.text = size_f
+	elif file_selecting == 5:
+		$MarginContainer/VBoxContainer/HBoxContainer5/LineEdit.text = path
+		$MarginContainer/VBoxContainer/HBoxContainer5/LineEdit2.text = size_f
 
 func _on_Button_pressed():
 	file_selecting = 1
-	
 	$FileDialog.popup_centered()
-	pass # Replace with function body.
 
 
 func _on_Button2_pressed():
 	file_selecting = 2
 	$FileDialog.popup_centered()
-	pass # Replace with function body.
+
+
+func _on_Button3_pressed():
+	file_selecting = 3
+	$FileDialog.popup_centered()
+
+
+func _on_Button4_pressed():
+	file_selecting = 4
+	$FileDialog.popup_centered()
+
+
+func _on_Button5_pressed():
+	file_selecting = 5
+	$FileDialog.popup_centered()
